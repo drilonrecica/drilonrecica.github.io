33397b27b2ba7cc328d4b5099d4c1e46741370c2
Drilon Reçica <drilonrecica.dev@gmail.com>
2026-01-06 21:23:18 +0100

    Configure site and base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://drilonrecica.github.io',
+  base: '/', // Set to '/<repo>/' if deploying to a project page
   vite: {
     plugins: [tailwindcss()]
   },
